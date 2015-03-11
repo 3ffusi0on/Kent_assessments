049e758cda55948d0cfa26d5536e0d974d130e56
Pierre-Alain SIMON <simon_p@epitech.eu>
2015-03-11 11:45:50 +0000

    Add assessment 3 of Erlang

---
+++ Erlang/assessment3.erl
@@ -0,0 +1,22 @@
+-module(assessment3).
+-compile([export_all]).
+
+%%
+%% The Dining Philosophers
+%%
+
+% Manager process
+college() ->
+    True
+
+% Output process
+report() ->
+    True
+
+% Stick/Fork process
+fork() ->
+    True
+
+% Philosopher process
+philosopher() ->
+    True
